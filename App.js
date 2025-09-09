0686d34c7dd5dd71b41de0ebbb709a361bc6deda
Felipewxyz <159555476+Felipewxyz@users.noreply.github.com>
2025-09-09 01:05:50 +0000

    [feat]Aula08/09

--- App.js
+++ App.js
@@ -1,34 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, Button, StyleSheet, Text, View } from 'react-native';
 
 
 export default function App() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  const fetchData = async () => {
-    const API_URL = "https://curly-space-trout-4vjgjx6qwq5hqp66-3000.app.github.dev"
-    const ENDPOINT = "/api/user"
-    const response = await fetch(API_URL + ENDPOINT);
+  const fetchDataGet = async () => {
+    const API = "http://192.168.0.109:3000/api";
+    const URL = `${API}/user`;
+    setLoading(true);
+
+    try {
+      const response = await fetch(URL);
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const fetchDataPost = async () => {
+    const API_URL = "http://192.168.0.109:3000"; // Corrigido
+    const ENDPOINT = "/api/user";
+    const body = {
+      username: "usuario1",
+      email: "usuario@teste.com.br"
+    };
+    const response = await fetch(API_URL + ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json' // Corrigido
+      },
+      body: JSON.stringify(body)
+    });
     const json = await response.json();
-    setData(json);
+    console.log(json);
   }
 
   return (
     <View style={styles.container}>
-      <Text>Carregar dados do servidor</Text>
-      {
-        data?.map(item => {
-          return (
-            <Text key={item.id}>{item.username}</Text>
-          )
-        })
-      }
-      <Button title="Carregar Dados" onPress={fetchData}></Button>
-            <StatusBar style="auto" /> 
+      <Text>Projeto TCC - Turma B</Text>
+      <View>
+        {loading ? (
+          <ActivityIndicator />
+        ) : (
+          <Button title="Buscar Dados" onPress={fetchDataGet}></Button>
+        )}
+      </View>
+      <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
